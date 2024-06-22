f6994e0919cf7a3775785e3d9514e3c888aa5108
Ignacio Vallarino <ignaciovallarinocibils@gmail.com>
2024-06-22 20:23:15 -0300

    feat: Being able to show a coffee image!

--- lib/presentation/widgets/coffee_image_widget.dart
+++ lib/presentation/widgets/coffee_image_widget.dart
@@ -2,7 +2,6 @@ import 'dart:io';
 
 import 'package:coffee_app/models/coffee_photo.dart';
 import 'package:flutter/material.dart';
-import 'package:image/image.dart' as img;
 
 class CoffeeImageWidget extends StatelessWidget {
   final CoffeePhoto coffeePhoto;
@@ -33,22 +32,6 @@ class CoffeeImageWidget extends StatelessWidget {
                   fit: BoxFit.cover,
                 ),
               ),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  Container(
-                    color: Colors.black.withOpacity(0.5),
-                    padding: const EdgeInsets.all(8.0),
-                    child: const Text(
-                      "Gola",
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 20,
-                      ),
-                    ),
-                  ),
-                ],
-              ),
             );
           }
         },
@@ -57,28 +40,6 @@ class CoffeeImageWidget extends StatelessWidget {
   }
 
   Future<File> _loadImageFile(File file) async {
-    // Check if the file exists
-    if (!await file.exists()) {
-      throw Exception('File does not exist');
-    }
-
-    // Check if the file is not empty
-    if (await file.length() == 0) {
-      throw Exception('File is empty');
-    }
-
-    // Check if the file is a valid image
-    try {
-      final bytes = await file.readAsBytes();
-      final image = img.decodeImage(bytes);
-      if (image == null) {
-        throw Exception('Invalid image file');
-      }
-    } catch (e) {
-      throw Exception('Error decoding image file: $e');
-    }
-
-    // Simulate a delay for loading the image file
     await Future.delayed(const Duration(milliseconds: 500));
     return file;
   }
--- lib/services/api/get_coffee_images_api.dart
+++ lib/services/api/get_coffee_images_api.dart
@@ -1,16 +1,40 @@
 import 'dart:io';
 
 import 'package:http/http.dart' as http;
+import 'package:image/image.dart' as img;
 import 'package:path_provider/path_provider.dart';
 
 class GetCoffeeImagesAPI {
-  static const String _api = 'https://coffee.alexflipnote.dev';
+  static const String _api = 'https://coffee.alexflipnote.dev/random';
 
   Future<File> getCoffeePhoto() async {
     final response = await http.get(Uri.parse(_api));
+
+    // Check if the HTTP request was successful
+    if (response.statusCode != 200) {
+      throw Exception('Failed to load image');
+    }
+
+    // Check if the response body is not empty
+    if (response.bodyBytes.isEmpty) {
+      throw Exception('Image data is empty');
+    }
+
+    // Check if the response body contains valid image data
+    final image = img.decodeImage(response.bodyBytes);
+    if (image == null) {
+      throw Exception('Invalid image data');
+    }
+
+    // Get the application documents directory
     final documentDirectory = await getApplicationDocumentsDirectory();
-    final file = File('${documentDirectory.path}/coffee_image.jpg');
+
+    // Create a file to save the image
+    final file = File('${documentDirectory.path}/coffee_image2.jpg');
+
+    // Write the image data to the file
     await file.writeAsBytes(response.bodyBytes);
+
     return file;
   }
 }
